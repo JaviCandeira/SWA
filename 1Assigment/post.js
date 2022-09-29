9eea0fb9dd59511e82e1e7037713f0e296d5e7a9
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2022-09-29 11:02:43 +0200

    Everythng works

--- 1Assigment/post.js
+++ 1Assigment/post.js
@@ -12,3 +12,22 @@ const post = () => {
     };
     xhr.send(jsonTxt);
 }
+
+const postFetch = () => {
+    const inputText = document.getElementById('inputText').value;
+    const jsonObj = JSON.parse(inputText);
+    const jsonTxt = JSON.stringify(jsonObj);
+
+    fetch('http://localhost:8080/data', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json',},
+        body: jsonTxt,
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log('Success:', data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}

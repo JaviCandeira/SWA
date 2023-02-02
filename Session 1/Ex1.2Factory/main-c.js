0fb3c3958b674ac1257fb76c87ef173b129c79b7
Javier Candeira <56227361+JaviCandeira@users.noreply.github.com>
2023-02-02 10:40:04 +0100

    Delete Session 1 directory

--- Session 1/Ex1.2Factory/main-c.js
+++
@@ -1,9 +0,0 @@
-import Circle from "./Circle.js";
-import Point from "./Point.js";
-
-let circle1 = Circle(null, 5,1,2);
-console.log(circle1.toString());
-
-let point1 = Point(3, 4);
-let circle2 = Circle(point1, 5);
-console.log(circle2.toString());
